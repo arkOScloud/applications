c10a2394c4283e63aa084f040882d820c9c6280f
Jacob Cook <jacob@peakwinter.net>
2015-09-12 17:02:50 -0400

    minor fix to oc restore

--- owncloud/backup.py
+++ owncloud/backup.py
@@ -6,7 +6,7 @@ from arkos.backup import BackupController
 class ownCloudBackup(BackupController):
     def get_config(self, site):
         return []
-    
+
     def get_data(self, site):
         datadir = None
         if os.path.exists(os.path.join(site.path, 'config', 'config.php')):
@@ -17,16 +17,16 @@ class ownCloudBackup(BackupController):
                         datadir = data[1]
         if datadir:
             return [datadir]
-    
+
     def pre_backup(self, site):
         pass
-    
+
     def post_backup(self, site):
         pass
-    
-    def pre_restore(self, site):
+
+    def pre_restore(self):
         pass
-    
+
     def post_restore(self, site, dbpasswd):
         with open(os.path.join(site.path, 'config', 'config.php'), 'r') as f:
             data = f.readlines()
