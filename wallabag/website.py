876c34d6359fb97b837c431f2eeacd1997066d98
Jacob Cook <jacob@peakwinter.net>
2016-10-01 18:47:05 -0400

    wallabag 2.x

--- wallabag/website.py
+++ wallabag/website.py
@@ -1,8 +1,8 @@
-import hashlib
 import nginx
 import os
 import shutil
 
+from arkos import logger
 from arkos.languages import php
 from arkos.websites import Site
 from arkos.utilities import errors, shell, random_string
@@ -12,153 +12,89 @@ from arkos.system import users, groups
 class Wallabag(Site):
     addtoblock = [
         nginx.Location(
-            '~ /(db)',
-            nginx.Key('deny', 'all'),
-            nginx.Key('return', '404')
+            "/",
+            nginx.Key("try_files", "$uri /app.php$is_args$args")
         ),
         nginx.Location(
-            '= /favicon.ico',
-            nginx.Key('log_not_found', 'off'),
-            nginx.Key('access_log', 'off')
-        ),
-        nginx.Location(
-            '= /robots.txt',
-            nginx.Key('allow', 'all'),
-            nginx.Key('log_not_found', 'off'),
-            nginx.Key('access_log', 'off')
-        ),
-        nginx.Location(
-            '/',
-            nginx.Key('try_files', '$uri $uri/ /index.php?$args')
-        ),
-        nginx.Location(
-            '~ \.php$',
+            "~ ^/app\.php(/|$)",
             nginx.Key('fastcgi_pass', 'unix:/run/php-fpm/php-fpm.sock'),
-            nginx.Key('fastcgi_index', 'index.php'),
-            nginx.Key('include', 'fastcgi.conf')
-        ),
-        nginx.Location(
-            '~* \.(js|css|png|jpg|jpeg|gif|ico)$',
-            nginx.Key('expires', 'max'),
-            nginx.Key('log_not_found', 'off')
+            nginx.Key('fastcgi_split_path_info', "^(.+\.php)(/.*)$"),
+            nginx.Key('include', 'fastcgi.conf'),
+            nginx.Key(
+                'fastcgi_param',
+                'SCRIPT_FILENAME $realpath_root$fastcgi_script_name'
+            ),
+            nginx.Key('fastcgi_param', 'DOCUMENT_ROOT $realpath_root')
         )
     ]
 
     def pre_install(self, extra_vars):
-        if not extra_vars.get('wb-username'):
-            raise errors.InvalidConfigError(
-                'Must choose a Wallabag username'
-            )
-        elif not extra_vars.get('wb-passwd'):
-            raise errors.InvalidConfigError(
-                'Must choose a Wallabag password'
-            )
-        elif '"' in extra_vars.get('wb-passwd')\
-                or "'" in extra_vars.get('wb-passwd'):
-            raise errors.InvalidConfigError(
-                'Your Wallabag password must not include quotes'
-            )
+        pass
 
     def post_install(self, extra_vars, dbpasswd=""):
         secret_key = random_string()
         dbengine = 'mysql' \
-            if self.meta.selected_dbengine == 'db-mariadb' \
+            if self.app.selected_dbengine == 'db-mariadb' \
             else 'sqlite'
 
-        username = extra_vars.get("wb-username")
-        passwd = extra_vars.get("wb-passwd") + username + secret_key
-        passwd = hashlib.sha1(passwd).hexdigest()
-
         # Write a standard Wallabag config file
-        shutil.copy(os.path.join(
-            self.path,
-            'inc/poche/config.inc.default.php'),
-            os.path.join(self.path, 'inc/poche/config.inc.php'))
-        with open(os.path.join(
-                self.path,
-                'inc/poche/config.inc.php'), 'r') as f:
+        config_file = os.path.join(self.path, 'app/config/parameters.yml')
+        with open(config_file + ".dist", 'r') as f:
             ic = f.readlines()
-        oc = []
-        for l in ic:
-            if 'define (\'SALT\'' in l:
-                l = '@define (\'SALT\', \'{0}\');\n'.format(secret_key)
-                oc.append(l)
-            elif 'define (\'STORAGE\'' in l:
-                l = '@define (\'STORAGE\', \'{0}\');\n'.format(dbengine)
-                oc.append(l)
-            elif 'define (\'STORAGE_SQLITE\'' in l and dbengine == 'sqlite':
-                l = '@define (\'STORAGE_SQLITE\', '\
-                    '\'/var/lib/sqlite3/{0}.db\');\n'.format(self.db.id)
-                oc.append(l)
-            elif 'define (\'STORAGE_DB\'' in l and dbengine == 'mysql':
-                l = "@define ('STORAGE_DB', '{0}');\n".format(self.db.id)
-                oc.append(l)
-            elif 'define (\'STORAGE_USER\'' in l and dbengine == 'mysql':
-                l = "@define ('STORAGE_USER', '{0}');\n".format(self.db.id)
-                oc.append(l)
-            elif 'define (\'STORAGE_PASSWORD\'' in l and dbengine == 'mysql':
-                l = '@define (\'STORAGE_PASSWORD\', '\
-                    '\'{0}\');\n'.format(dbpasswd)
-                oc.append(l)
-            else:
-                oc.append(l)
-        with open(os.path.join(
-                self.path,
-                'inc/poche/config.inc.php'), 'w') as f:
-            f.writelines(oc)
+        with open(config_file, 'w') as f:
+            for l in ic:
+                if "database_driver: " in l:
+                    pdo = "pdo_mysql" if dbengine == "mysql" else "pdo_sqlite"
+                    l = "    database_driver: {0}\n".format(pdo)
+                elif "database_path: " in l and dbengine == 'sqlite':
+                    l = "    database_path: {0}\n".format(self.db.path)
+                elif "database_name: " in l and dbengine == 'mysql':
+                    l = "    database_name: {0}\n".format(self.db.id)
+                elif "database_user: " in l and dbengine == 'mysql':
+                    l = "    database_user: {0}\n".format(self.db.id)
+                elif "database_password: " in l and dbengine == 'mysql':
+                    l = '    database_password: "{0}"\n'.format(dbpasswd)
+                elif "secret: " in l:
+                    l = "    secret: {0}\n".format(secret_key)
+                f.write(l)
 
         # Make sure that the correct PHP settings are enabled
         php.enable_mod('sqlite3',
                        'pdo_mysql' if dbengine == 'mysql' else 'pdo_sqlite',
                        'zip', 'tidy')
+        php.open_basedir('add', '/usr/bin/php')
 
         # Set up Composer and install the proper modules
-        php.composer_install(self.path)
+        php.composer_install(
+            self.path,
+            flags={"no-dev": "", "o": "", "prefer-dist": ""},
+            env={"SYMFONY_ENV": "prod"}
+        )
 
         uid, gid = users.get_system("http").uid, groups.get_system("http").gid
 
         # Set up the database then delete the install folder
-        if dbengine == 'mysql':
-            with open(os.path.join(self.path, 'install/mysql.sql')) as f:
-                self.db.execute(f.read(), commit=True)
-            self.db.execute(
-                "INSERT INTO users (username, password, name, email) "
-                "VALUES ('{0}', '{1}', '{2}', '');"
-                .format(username, passwd, username),
-                commit=True)
-            lid = int(self.db.manager.connection.insert_id())
-            self.db.execute(
-                "INSERT INTO users_config (user_id, name, value) "
-                "VALUES ({0}, 'pager', '10');".format(lid),
-                commit=True)
-            self.db.execute(
-                "INSERT INTO users_config (user_id, name, value) "
-                "VALUES ({0}, 'language', 'en_EN.UTF8');".format(lid),
-                commit=True)
-        else:
-            shutil.copy(os.path.join(self.path, 'install/poche.sqlite'),
-                        '/var/lib/sqlite3/{0}.db'.format(self.db.id))
+        if dbengine == 'sqlite3':
             php.open_basedir('add', '/var/lib/sqlite3')
+
+        cwd = os.getcwd()
+        os.chdir(self.path)
+        s = shell("php bin/console wallabag:install --env=prod -n")
+        if s["code"] != 0:
+            logger.error(s["stderr"].decode())
+            raise errors.OperationFailedError(
+                "Failed to populate database. See logs for more info"
+            )
+        os.chdir(cwd)
+
+        if dbengine == 'sqlite3':
             os.chown("/var/lib/sqlite3/{0}.db".format(self.db.id), -1, gid)
-            os.chmod("/var/lib/sqlite3/{0}.db".format(self.db.id), 0o664)
-            self.db.execute(
-                "INSERT INTO users (username, password, name, email) "
-                "VALUES ('{0}', '{1}', '{2}', '');"
-                .format(username, passwd, username))
-            self.db.execute(
-                "INSERT INTO users_config (user_id, name, value) "
-                "VALUES (1, 'pager', '10');")
-            self.db.execute(
-                "INSERT INTO users_config (user_id, name, value) "
-                "VALUES (1, 'language', 'en_EN.UTF8');")
-        shutil.rmtree(os.path.join(self.path, 'install'))
+            os.chmod("/var/lib/sqlite3/{0}.db".format(self.db.id), 0o660)
 
         # Finally, make sure that permissions are set so that Wallabag
         # can make adjustments and save plugins when need be.
         for r, d, f in os.walk(self.path):
             for x in d:
-                if d in ["assets", "cache", "db"]:
-                    os.chmod(os.path.join(r, d), 0o755)
                 os.chown(os.path.join(r, x), uid, gid)
             for x in f:
                 os.chown(os.path.join(r, x), uid, gid)
